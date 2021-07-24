f6d0f63663c8fc9c64fa34866d7f7677df75dee2
Jose Marino <jose.marino@dev.null>
2021-07-24 15:06:24 -0600

    add Epub class
    Mode most of code inside Epub class.

--- epubseries.py
+++ epubseries.py
@@ -4,23 +4,272 @@ import argparse
 import zipfile
 import xml.etree.ElementTree as ET
 import re
+import copy
+
+
+#NOTE: see 2034 kepub
+#**TODO** remove old series info before adding new one
 
 
 settings = {'epub3': False,
             'calibre': True
             }
 
-# Namespaces
-NS = {'ns0': 'http://www.idpf.org/2007/opf',
-      'dc': 'http://purl.org/dc/elements/1.1/'}
+
+class Epub:
+    # Namespaces
+    NS = {'ns0': 'http://www.idpf.org/2007/opf',
+          'dc': 'http://purl.org/dc/elements/1.1/'}
+
+    def __init__(self, fileName):
+        self._fileName = fileName
+        self._contentFileName = None
+        self._xml = None               # ElementTree main object
+        self._metadataNode = None      # metadata node
+        self._version = None
+    #__init__
+
+    def findContent(self, zfile):
+        """Find location of 'content.opf' file. This is stored in 'META-INF/container.xml'"""
+        with zfile.open("META-INF/container.xml") as file:
+            xml = ET.fromstring(file.read())
+        #with
+
+        self._contentFileName = xml[0][0].attrib['full-path']        # <container> <rootfiles> <rootfile full-path="...">
+    #findContent
+
+    def readFile(self):
+        try:
+            zfile = zipfile.ZipFile(self._fileName)
+            self.findContent(zfile)
+            #print(f"Found: {self._fileName}")
+        except FileNotFoundError:
+            print(f"ERROR: File '{self._fileName}' not found. Bailing out.")
+            raise FileNotFoundError
+        #try
+
+        with zfile.open(self._contentFileName) as file:
+            self._xml = ET.fromstring(file.read())
+            #with
+        zfile.close()
+
+        self._metadataNode = self._xml.find('ns0:metadata', namespaces=Epub.NS)
+        if self._metadataNode == None:
+            raise Exception("Can't find metadata object")
+        #
+
+        if 'version' in self._xml.attrib:
+            self._version = self._xml.attrib['version']
+        #if
+    #readFile
+
+    def saveFile(self):
+        elem_tree = ET.ElementTree(self._xml)
+
+        new_zip_filename = self._fileName + '.new'
+        # create a temp copy of the archive without filename
+        with zipfile.ZipFile(self._fileName, 'r') as zin:
+            with zipfile.ZipFile(new_zip_filename, 'w') as zout:
+                zout.comment = zin.comment # preserve the comment
+                for item in zin.infolist():
+                    if item.filename != self._contentFileName:
+                        zout.writestr(item, zin.read(item.filename))
+                    #
+                #for
+            #with
+        #with
+
+        with zipfile.ZipFile(new_zip_filename, 'a', compression=zipfile.ZIP_DEFLATED) as zout:
+            with zout.open(self._contentFileName, 'w') as file:
+                elem_tree.write(file)
+            #
+        #with
+        print(f"New epub file: '{new_zip_filename}'")
+    #saveFile
+
+    def printMetadata(self):
+        tree = ET.ElementTree(self._xml)
+        print(f'File: {self._fileName}')
+        metadataNode = copy.deepcopy(self._metadataNode)
+        ET.indent(metadataNode)
+        print('metadata:')
+        print(ET.tostring(metadataNode, encoding='unicode'))
+    #printMetadata
+
+    def getTitle(self):
+        titleNode = self._metadataNode.find('dc:title', namespaces=Epub.NS)
+        if titleNode == None:
+            print("WARNING: unable to find title")
+            return ""
+        #
+        return titleNode.text
+    #getTitle
+
+    def getAuthors(self):
+        authorNodes = self._xml.findall('ns0:metadata/dc:creator', namespaces=Epub.NS)
+        if len(authorNodes) == 0:
+            print("WARNING: unable to find authors")
+            return list()
+        #
+
+        authors = list()
+        for authorNode in authorNodes:
+            authors.append(authorNode.text)
+        #for
+
+        return authors
+    #getAuthors
+
+    def printInfo(self):
+        title = self.getTitle()
+        authors = self.getAuthors()
+
+        print(f'File: {self._fileName}')
+        print(f'  Format: ePub version {self._version}')
+        print(f'  Title: {title}')
+        if len(authors) == 1:
+            print(f'  Author: {authors[0]}')
+        else:
+            authorsStr = ", ".join(self._authors)
+            print(f'  Authors: {authorsStr}')
+        #if
+
+        calName_ele = self._metadataNode.find("meta[@name='calibre:series']")
+        calNumber_ele = self._metadataNode.find("meta[@name='calibre:series_index']")
+        calName = calName_ele.attrib['content'] if calName_ele!=None else ''
+        calNumber = calNumber_ele.attrib['content'] if calNumber_ele!=None else ''
+
+        epub3Name_ele = self._metadataNode.find("meta[@property='belongs-to-collection']")
+        epub3Number = ''
+        if epub3Name_ele:
+            seriesId = epub3Name_ele.attrib['id']
+            epub3Number_ele = self._metadataNode.find("meta[@refines='{}']".format(seriesId))
+            epub3Number = epub3Number_ele.text if epub3Number_ele!=None else ''
+        #fi
+        epub3Name = epub3Name_ele.text if epub3Name_ele!=None else ''
+
+        if len(calName) > 0:
+            print(f"  Series: {calName} : {calNumber}  (calibre)")
+        #if
+        if len(epub3Name) > 0:
+            print(f"  Series: {epub3Name} : {epub3Number}  (epub3)")
+        #if
+    #printInfo
+
+    def deleteRefines(self, idName):
+        if idName == None:
+            return
+
+        for refineNode in self._metadataNode.findall(f'.//*[@refines="#{idName}"]'):
+            self._metadataNode.remove(refineNode)
+        #if
+    #deleteRefines
+
+    def setTitle(self, newTitle):
+        oldTitles = self._xml.findall('ns0:metadata/dc:title', namespaces=Epub.NS)
+        if len(oldTitles) == 0:
+            raise Exception("Can't find title in metadata")
+        #
+
+        if len(oldTitles) > 1:
+            print(f'WARNING: found {len(oldTitles)} in metadata section')
+        #if
+
+        # delete old title(s)
+        nodeIndex = list(self._metadataNode).index(oldTitles[0])
+        for titleNode in oldTitles:
+            # delete any nodes that refine this title
+            titleId = titleNode.attrib['id'] if 'id' in titleNode.attrib else None
+            self.deleteRefines(titleId)
+
+            # delete title node
+            self._metadataNode.remove(titleNode)
+        #for
+
+        # add new title node:
+        #   <dc:title>Title</dc:title>
+        titleNode = ET.Element('{%s}title' % Epub.NS['dc'])
+        titleNode.text = newTitle
+        titleNode.tail = '\n'
+        self._metadataNode.insert(nodeIndex, titleNode)
+    #setTitle
+
+    def setAuthor(xml, newAuthors):
+        oldAuthors = self._xml.findall('ns0:metadata/dc:creator', namespaces=Epub.NS)
+        if len(oldAuthors) == 0:
+            raise Exception("Can't find authors in metadata")
+        #
+
+        # delete old authors
+        nodeIndex = list(self._metadataNode).index(oldAuthors[0])
+        for authorNode in oldAuthors:
+            # delete any nodes that refine this author: with attrib refines="#creator01"
+            authorId = authorNode.attrib['id'] if 'id' in authorNode.attrib else None
+            self.deleteRefines(authorId)
+
+            # delete author node
+            self._metadataNode.remove(authorNode)
+        #for
+
+        # add new author nodes:
+        #   <dc:creator>Name</dc:creator>
+        #   <ns0:meta refines="id">aut</ns0:meta>
+        for i in range(len(newAuthors)):
+            authorId = f'creator{(i+1):02}'
+            authorName = newAuthors[i]
+            authorNode = ET.Element('{%s}creator' % Epub.NS['dc'], attrib={'id': authorId})
+            authorNode.text = authorName
+            authorNode.tail = '\n'
+            self._metadataNode.insert(nodeIndex, authorNode)
+            nodeIndex += 1
+
+            metaNode = ET.Element('{%s}meta' % Epub.NS['ns0'], attrib={'id': 'role', 'refines': f'#{authorId}'})
+            metaNode.text = 'aut'
+            metaNode.tail = '\n'
+            self._metadataNode.insert(nodeIndex, metaNode)
+            nodeIndex += 1
+        #for
+    #setAuthor
+
+    def setSeriesInfo(self, seriesTitle, seriesNumber):
+        """Find <metadata> element and add series info at the end."""
+
+        # add series info to metadata
+        # <meta property="belongs-to-collection" id="c01">
+        #     The Lord of the Rings
+        # </meta>
+        # <meta refines="#c01" property="collection-type">set</meta>
+        # <meta refines="#c01" property="group-position">2</meta>
+        if settings['epub3']:
+            meta = ET.SubElement(self._metadataNode, 'meta', {'property': 'belongs-to-collection', 'id':'series0'})
+            meta.text = seriesTitle
+            meta = ET.SubElement(self._metadataNode, 'meta', {'refines': '#series0', 'property': 'collection-type'})
+            meta.text = 'set'
+            meta = ET.SubElement(self._metadataNode, 'meta', {'refines': '#series0', 'property': 'group-position'})
+            meta.text = f'{seriesNumber}'
+        #if
+
+        # Add series info to metadata in Calibre format
+        # <meta name="calibre:series" content="The Lord of the Rings"/>
+        # <meta name="calibre:series_index" content="2"/>
+        if settings['calibre']:
+            meta = ET.SubElement(self._metadataNode, 'meta', {'name': 'calibre:series', 'content': seriesTitle})
+            meta = ET.SubElement(self._metadataNode, 'meta', {'name': 'calibre:series_index', 'content': seriesNumber})
+        #if
+    #setSeriesInfo
+
+#Epub
 
 
 def handleParameters():
     desc = """Update series metadata in epub. By default it uses calibre metadata format.
 Writes output to new .epub.new file."""
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('seriesinfo', type=str, help='Series info with format "Series Name:2"')
     parser.add_argument('filename', type=str, help='epub file')
+    parser.add_argument('-m', '--metadata', action='store_true', help='Dump xml metadata')
+    parser.add_argument('-i', '--info',  action='store_true', help='Print info about ebook')
+    parser.add_argument('-s', '--series', type=str, help='Set series info with format "Series Name:2"')
+    parser.add_argument('-t', '--title', type=str, help='Set title')
     parser.add_argument('-c', '--calibre', action='store_true', help='use Calibre series metadata format (default)')
     parser.add_argument('-3', '--epub3', action='store_true', help='use EPUB3 series metadata format')
     args = parser.parse_args()
@@ -40,208 +289,14 @@ def parseSeries(series_data):
 #
 
 
-def findContent(zfile):
-    """Find location of 'content.opf' file. This is stored in 'META-INF/container.xml'"""
-    with zfile.open("META-INF/container.xml") as file:
-        xml = ET.fromstring(file.read())
-    #with
-
-    return xml[0][0].attrib['full-path']        # <container> <rootfiles> <rootfile full-path="...">
-#findContent
-
-def readEpubFile(fileName):
-    try:
-        zfile = zipfile.ZipFile(fileName)
-        contentFileName = findContent(zfile)
-        #print(f"Found: {fileName}")
-    except FileNotFoundError:
-        print(f"ERROR: File '{fileName}' not found. Bailing out.")
-        return
-    #try
-
-    with zfile.open(contentFileName) as file:
-        xml = ET.fromstring(file.read())
-    #with
-    zfile.close()
-
-    return xml, contentFileName
-#readEpubFile
-
-def printMetadata(xml):
-    metadata = xml.find('ns0:metadata', namespaces=NS)
-    if metadata == None:
-        raise Exception("Can't find metadata object")
-    #
-    title = metadata.find('dc:title', namespaces=NS)
-    authors = [a.text for a in metadata.findall('dc:creator', namespaces=NS)]
-
-    print('Title: {}'.format(title.text))
-    print('Author: {}'.format(", ".join(authors)))
-
-    calName_ele = metadata.find("meta[@name='calibre:series']")
-    calNumber_ele = metadata.find("meta[@name='calibre:series_index']")
-    calName = calName_ele.attrib['content'] if calName_ele!=None else ''
-    calNumber = calNumber_ele.attrib['content'] if calNumber_ele!=None else ''
-
-    epub3Name_ele = metadata.find("meta[@property='belongs-to-collection']")
-    epub3Number = ''
-    if epub3Name_ele:
-        seriesId = epub3Name_ele.attrib['id']
-        epub3Number_ele = metadata.find("meta[@refines='{}']".format(seriesId))
-        epub3Number = epub3Number_ele.text if epub3Number_ele!=None else ''
-    #fi
-    epub3Name = epub3Name_ele.text if epub3Name_ele!=None else ''
-
-    if len(calName) > 0:
-        print("Series: {} : {}  (calibre)".format(calName, calNumber))
-    #if
-    if len(epub3Name) > 0:
-        print("Series: {} : {}  (calibre)".format(epub3Name, epub3Number))
-    #if
-
-#printMetadata
-
-
-def addSeriesToMetadata(xml, series_title, series_number):
-    """Find <metadata> element and add series info at the end."""
-
-    # extract namespace from root element
-    namespace = re.match(r'{.*}', xml.tag).group(0)  # namespace='{http://www.idpf.org/2007/opf}'
-    ns = {'root': namespace[1:-1]}   # without the { }
-    # now we can find the 'metadata' object
-    metadata = xml.find('root:metadata', ns)
-    if metadata == None:
-        raise Exception("Can't find metadata object")
-    #
-
-    # add series info to metadata
-    # <meta property="belongs-to-collection" id="c01">
-    #     The Lord of the Rings
-    # </meta>
-    # <meta refines="#c01" property="collection-type">set</meta>
-    # <meta refines="#c01" property="group-position">2</meta>
-    if settings['epub3']:
-        meta = ET.SubElement(metadata, 'meta', {'property': 'belongs-to-collection', 'id':'series0'})
-        meta.text = series_title
-        meta = ET.SubElement(metadata, 'meta', {'refines': '#series0', 'property': 'collection-type'})
-        meta.text = 'set'
-        meta = ET.SubElement(metadata, 'meta', {'refines': '#series0', 'property': 'group-position'})
-        meta.text = f'{series_number}'
-
-    # Add series info to metadata in Calibre format
-    # <meta name="calibre:series" content="The Lord of the Rings"/>
-    # <meta name="calibre:series_index" content="2"/>
-    if settings['calibre']:
-        meta = ET.SubElement(metadata, 'meta', {'name': 'calibre:series', 'content': series_title})
-        meta = ET.SubElement(metadata, 'meta', {'name': 'calibre:series_index', 'content': series_number})
-#
-
-
-def updateZipFile(zip_filename, contentFileName, xml):
-    elem_tree = ET.ElementTree(xml)
-
-    new_zip_filename = zip_filename + '.new'
-    # create a temp copy of the archive without filename
-    with zipfile.ZipFile(zip_filename, 'r') as zin:
-        with zipfile.ZipFile(new_zip_filename, 'w') as zout:
-            zout.comment = zin.comment # preserve the comment
-            for item in zin.infolist():
-                if item.filename != contentFileName:
-                    zout.writestr(item, zin.read(item.filename))
-                #
-            #for
-        #with
-    #with
-
-    with zipfile.ZipFile(new_zip_filename, 'a', compression=zipfile.ZIP_DEFLATED) as zout:
-        with zout.open(contentFileName, 'w') as file:
-            elem_tree.write(file)
-        #
-    #
-    print(f"New epub file: '{new_zip_filename}'")
-#
-
-def deleteRefines(metadataNode, idName):
-    if idName == None:
-        return
-
-    for refineNode in metadataNode.findall(f'.//*[@refines="#{idName}"]'):
-        metadataNode.remove(refineNode)
-    #if
-#deleteRefines
-
-def setTitle(xml, newTitle):
-    metadata = xml.find('ns0:metadata', namespaces=NS)
-    oldTitles = xml.findall('ns0:metadata/dc:title', namespaces=NS)
-    if metadata == None or len(oldTitles) == 0:
-        raise Exception("Can't find title in metadata")
-    #
-
-    if len(oldTitles) > 1:
-        print(f'WARNING: found {len(oldTitles)} in metadata section')
-    #if
-
-    # delete old title(s)
-    nodeIndex = list(metadata).index(oldTitles[0])
-    for titleNode in oldTitles:
-        # delete any nodes that refine this title
-        titleId = titleNode.attrib['id'] if 'id' in titleNode.attrib else None
-        deleteRefines(metadata, titleId)
-
-        # delete title node
-        metadata.remove(titleNode)
-    #for
-
-    # add new title node:
-    #   <dc:title>Title</dc:title>
-    titleNode = ET.Element('{%s}title' % NS['dc'])
-    titleNode.text = newTitle
-    titleNode.tail = '\n'
-    metadata.insert(nodeIndex, titleNode)
-#setTitle
-
-def setAuthor(xml, newAuthors):
-    metadata = xml.find('ns0:metadata', namespaces=NS)
-    oldAuthors = xml.findall('ns0:metadata/dc:creator', namespaces=NS)
-    if metadata == None or len(oldAuthors) == 0:
-        raise Exception("Can't find authors in metadata")
-    #
-
-    # delete old authors
-    nodeIndex = list(metadata).index(oldAuthors[0])
-    for authorNode in oldAuthors:
-        # delete any nodes that refine this author: with attrib refines="#creator01"
-        authorId = authorNode.attrib['id'] if 'id' in authorNode.attrib else None
-        deleteRefines(metadata, authorId)
-
-        # delete author node
-        metadata.remove(authorNode)
-    #for
-
-    # add new author nodes:
-    #   <dc:creator>Name</dc:creator>
-    #   <ns0:meta refines="id">aut</ns0:meta>
-    for i in range(len(newAuthors)):
-        authorId = f'creator{(i+1):02}'
-        authorName = newAuthors[i]
-        authorNode = ET.Element('{%s}creator' % NS['dc'], attrib={'id': authorId})
-        authorNode.text = authorName
-        authorNode.tail = '\n'
-        metadata.insert(nodeIndex, authorNode)
-        nodeIndex += 1
-
-        metaNode = ET.Element('{%s}meta' % NS['ns0'], attrib={'id': 'role', 'refines': f'#{authorId}'})
-        metaNode.text = 'aut'
-        metaNode.tail = '\n'
-        metadata.insert(nodeIndex, metaNode)
-        nodeIndex += 1
-    #for
-#setAuthor
-
 def main():
     args = handleParameters()
     epub_filename = args.filename
-    series_title, series_number = parseSeries(args.seriesinfo)
+
+    epub = Epub(epub_filename)
+    epub.readFile()
+
+    fileModified = False
 
     if args.epub3:
         settings['epub3'] = True
@@ -252,20 +307,34 @@ def main():
         settings['calibre'] = True
     #
 
-    if series_number == None:
-        print(f"ERROR: couldn't parse series info: '{args.series}'")
-        return
-    #
+    if args.series != None:
+        series_title, series_number = parseSeries(args.series)
 
-    xml, contentFileName = readEpubFile(epub_filename)
+        if series_number == None:
+            print(f"ERROR: couldn't parse series info: '{args.series}'")
+            return
+        #if
 
+        epub.setSeriesInfo(series_title, series_number)
+        fileModified = True
+    #if
 
-    addSeriesToMetadata(xml, series_title, series_number)
+    if args.title != None:
+        epub.setTitle(args.title)
+        fileModified = True
+    #if
 
-    updateZipFile(epub_filename, contentFileName, xml)
-#
+    if args.info:      # -i
+        epub.printInfo()
+    #if
+    if args.metadata:  # -m
+        epub.printMetadata()
+    #if
 
-
+    if fileModified:
+        epub.saveFile()
+    #if
+#main
 
 if __name__ == "__main__":
     # execute only if run as a script
